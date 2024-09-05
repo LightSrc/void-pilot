81169a184cda65d6a4842ae0c5dec7914e804322
LightSrc <einarspiebalgs@gmail.com>
2024-09-05 21:28:06 +0300

    added librewolf

--- src/scripts/apps/browsers/librewolf_xbps.sh
+++ src/scripts/apps/browsers/librewolf_xbps.sh
@@ -1,4 +1,11 @@
 #!/bin/sh -e
-sleep 2
-echo "TODO"
-sleep 5
+REPO_FILE="/etc/xbps.d/librewolf-void.conf"
+REPOSITORY="https://github.com/index-0/librewolf-void/releases/latest/download/"
+
+if [ -e "$REPO_FILE" ]; then
+    echo "File $REPO_FILE already exists."
+else
+    echo "repository=$REPOSITORY" | sudo tee $REPO_FILE
+fi
+
+sudo xbps-install -Sy librewolf
