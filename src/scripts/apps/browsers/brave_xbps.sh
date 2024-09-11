ef9ea3a565bc1285d1654459313f97bfe52783aa
LightSrc <einarspiebalgs@gmail.com>
2024-09-11 13:29:10 +0300

    added brave

--- src/scripts/apps/browsers/brave_xbps.sh
+++ src/scripts/apps/browsers/brave_xbps.sh
@@ -1,4 +1,11 @@
 #!/bin/sh -e
-sleep 2
-echo "TODO"
-sleep 5
+REPO_FILE="/etc/xbps.d/brave-void.conf"
+REPOSITORY="https://gitlab.com/LightSrc/brave-void/-/raw/repository-x86_64-glibc"
+
+if [ -e "$REPO_FILE" ]; then
+    echo "File $REPO_FILE already exists."
+else
+    echo "repository=$REPOSITORY" | sudo tee $REPO_FILE
+fi
+
+sudo xbps-install -Sy brave
