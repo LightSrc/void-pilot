22fa4658f9a55333c30cc66962406a660e6ac0d6
LightSrc <einarspiebalgs@gmail.com>
2024-09-16 11:34:42 +0300

    implemented keybinding display bellow menu #3

---
+++ src/panels/keybindings.zig
@@ -0,0 +1,41 @@
+const std = @import("std");
+const vaxis = @import("vaxis");
+const menuModule = @import("menu.zig");
+
+pub fn drawKeybindings(win: *const vaxis.Window) !void {
+    if (menuModule.isMenuActive() == false) return;
+    const next_seg = vaxis.Cell.Segment{
+        .text = "Arrow Up/K = Up",
+        .style = .{ .bg = .{ .index = 0 } },
+    };
+    const prev_seg = vaxis.Cell.Segment{
+        .text = "   Arrow Down/J = Down",
+        .style = .{ .bg = .{ .index = 0 } },
+    };
+    const select_seg = vaxis.Cell.Segment{
+        .text = "   Enter = Select",
+        .style = .{ .bg = .{ .index = 0 } },
+    };
+    const back_seg = vaxis.Cell.Segment{
+        .text = "   Backspace = Back",
+        .style = .{ .bg = .{ .index = 0 } },
+    };
+    var bindings_segs = [_]vaxis.Cell.Segment{ next_seg, prev_seg, select_seg, back_seg };
+
+    var segs_text_len: usize = 0;
+    for (bindings_segs) |seg| {
+        segs_text_len += seg.text.len;
+    }
+    const child = win.*.initChild(
+        0,
+        win.*.height - 1,
+        .{ .limit = win.*.width },
+        .{ .limit = win.*.height / 2 },
+    );
+    const center_window = vaxis.widgets.alignment.center(
+        child,
+        segs_text_len,
+        child.height,
+    );
+    _ = try center_window.print(bindings_segs[0..], .{ .wrap = .none });
+}
