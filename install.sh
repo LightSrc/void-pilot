4c100ae03b6dc3bbf55213dc56c0356c2a125a2b
LightSrc <einarspiebalgs@gmail.com>
2024-11-20 13:21:01 +0200

    Added option to add void-pilot to PATH in install script

--- install.sh
+++ install.sh
@@ -17,4 +17,44 @@ tar xfz "void-pilot-x86_64-linux.tar.gz"
 rm void-pilot-x86_64-linux.tar.gz
 cd void-pilot
 chmod +x void-pilot
+
+echo "Do you want to add void-pilot to your PATH? yes(recommended), no"
+read add_to_path_answer
+
+if [ $add_to_path_answer = "y" ] || [ $add_to_path_answer = "yes" ]; then
+    if [ -f ~/.bashrc ]; then
+        if [ ! bashrc_contains_void_pilot_path ]; then
+            echo 'export PATH=$HOME/void-pilot:$PATH' >> ~/.bashrc
+            bash -c 'source ~/.bashrc'
+        else
+            echo ".bashrc already contains PATH for void-pilot, skipping..."
+        fi
+    fi
+
+    if [ -f ~/.zshrc ]; then
+        if [ ! zshrc_contains_dotnet_root ]; then
+            echo 'export PATH=$HOME/void-pilot:$PATH' >> ~/.zshrc
+            bash -c 'source ~/.zshrc'
+        else
+            echo ".zshrc already contains PATH for void-pilot, skipping..."
+        fi
+    fi
+fi
+
 ./void-pilot
+
+zshrc_contains_void_pilot_path() {
+    if grep -q '^export PATH=$HOME/void-pilot:$PATH$' ~/.zshrc; then
+        echo True
+    else
+        echo False
+    fi
+}
+
+bashrc_contains_void_pilot_path() {
+    if grep -q '^export PATH=$HOME/void-pilot:$PATH$' ~/.bashrc; then
+        echo True
+    else
+        echo False
+    fi
+}
