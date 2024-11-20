e8be15e06b1b6a692ffe87fe87b5c8d5f5f6f62c
LightSrc <einarspiebalgs@gmail.com>
2024-11-20 15:27:13 +0200

    improved install script with current dir instead of hardcoded home dir

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 DOWNLOAD_URL="https://github.com/LightSrc/void-pilot/releases/latest/download/void-pilot-x86_64-linux.tar.gz"
+CURRENT_WORK_DIR=$(pwd)
 
 findArch() {
     case "$(uname -m)" in
@@ -18,33 +19,28 @@ rm void-pilot-x86_64-linux.tar.gz
 cd void-pilot
 chmod +x void-pilot
 
-echo "Do you want to add void-pilot to your PATH? yes(recommended), no"
-read add_to_path_answer
-
-if [ "$add_to_path_answer" = "y" ] || [ "$add_to_path_answer" = "yes" ]; then
-    if [ -f ~/.bashrc ]; then
-        if [ "$(bashrc_contains_void_pilot_path)" = "False" ]; then
-            echo 'export PATH=$HOME/void-pilot:$PATH' >> ~/.bashrc
-            bash -c 'source ~/.bashrc'
-        else
-            echo ".bashrc already contains PATH for void-pilot, skipping..."
-        fi
+if [ -f ~/.bashrc ]; then
+    if [ "$(bashrc_contains_void_pilot_path)" = "False" ]; then
+        echo 'export PATH=$CURRENT_WORK_DIR/void-pilot:$PATH' >> ~/.bashrc
+        bash -c 'source ~/.bashrc'
+    else
+        echo ".bashrc already contains PATH for void-pilot, skipping..."
     fi
+fi
 
-    if [ -f ~/.zshrc ]; then
-        if [ "$(zshrc_contains_void_pilot_path)" = "False" ]; then
-            echo 'export PATH=$HOME/void-pilot:$PATH' >> ~/.zshrc
-            bash -c 'source ~/.zshrc'
-        else
-            echo ".zshrc already contains PATH for void-pilot, skipping..."
-        fi
+if [ -f ~/.zshrc ]; then
+    if [ "$(zshrc_contains_void_pilot_path)" = "False" ]; then
+        echo 'export PATH=$CURRENT_WORK_DIR/void-pilot:$PATH' >> ~/.zshrc
+        bash -c 'source ~/.zshrc'
+    else
+        echo ".zshrc already contains PATH for void-pilot, skipping..."
     fi
 fi
 
 ./void-pilot
 
 zshrc_contains_void_pilot_path() {
-    if grep -q '^export PATH=$HOME/void-pilot:$PATH$' ~/.zshrc; then
+    if grep -q '^export PATH=$CURRENT_WORK_DIR/void-pilot:$PATH$' ~/.zshrc; then
         echo True
     else
         echo False
@@ -52,7 +48,7 @@ zshrc_contains_void_pilot_path() {
 }
 
 bashrc_contains_void_pilot_path() {
-    if grep -q '^export PATH=$HOME/void-pilot:$PATH$' ~/.bashrc; then
+    if grep -q '^export PATH=$CURRENT_WORK_DIR/void-pilot:$PATH$' ~/.bashrc; then
         echo True
     else
         echo False
