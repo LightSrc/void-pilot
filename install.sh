d3570939a9bc4aca874406533ac0201577246385
LightSrc <einarspiebalgs@gmail.com>
2025-01-29 12:49:54 +0200

    forced $HOME dir for installation to not add multiple paths for void-pilot to shell rc files if user run install script in other dir by mistake

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 DOWNLOAD_URL="https://github.com/LightSrc/void-pilot/releases/latest/download/void-pilot-x86_64-linux.tar.gz"
-CURRENT_WORK_DIR=$(pwd)
 
 findArch() {
     case "$(uname -m)" in
@@ -14,7 +13,7 @@ if ! findArch; then
 fi
 
 zshrc_contains_void_pilot_path() {
-    if grep -q "^export PATH=$CURRENT_WORK_DIR/void-pilot:\$PATH$" ~/.zshrc; then
+    if grep -q "^export PATH=$HOME/void-pilot:\$PATH$" ~/.zshrc; then
         echo True
     else
         echo False
@@ -22,22 +21,25 @@ zshrc_contains_void_pilot_path() {
 }
 
 bashrc_contains_void_pilot_path() {
-    if grep -q "^export PATH=$CURRENT_WORK_DIR/void-pilot:\$PATH$" ~/.bashrc; then
+    if grep -q "^export PATH=$HOME/void-pilot:\$PATH$" ~/.bashrc; then
         echo True
     else
         echo False
     fi
 }
 
+if [ -d "$HOME/void-pilot" ]; then
+    rm -rf $HOME/void-pilot
+fi
+
 curl -fsSL "$DOWNLOAD_URL" -o void-pilot-x86_64-linux.tar.gz
-tar xfz "void-pilot-x86_64-linux.tar.gz"
+tar xfz "void-pilot-x86_64-linux.tar.gz" --directory $HOME
 rm void-pilot-x86_64-linux.tar.gz
-cd void-pilot
-chmod +x void-pilot
+chmod +x $HOME/void-pilot
 
 if [ -f ~/.bashrc ]; then
     if [ "$(bashrc_contains_void_pilot_path)" = "False" ]; then
-        echo "export PATH=$CURRENT_WORK_DIR/void-pilot:\$PATH" >> ~/.bashrc
+        echo "export PATH=$HOME/void-pilot:\$PATH" >> ~/.bashrc
         bash -c 'source ~/.bashrc'
     else
         echo ".bashrc already contains PATH for void-pilot, skipping..."
@@ -46,7 +48,7 @@ fi
 
 if [ -f ~/.zshrc ]; then
     if [ "$(zshrc_contains_void_pilot_path)" = "False" ]; then
-        echo "export PATH=$CURRENT_WORK_DIR/void-pilot:\$PATH" >> ~/.zshrc
+        echo "export PATH=$HOME/void-pilot:\$PATH" >> ~/.zshrc
         bash -c 'source ~/.zshrc'
     else
         echo ".zshrc already contains PATH for void-pilot, skipping..."
